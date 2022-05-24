67bea1bd5cb58a452316fe76ce56cf19a5eaf3bb
Corbin McGuire <mcguire.corbin@gmail.com>
2022-05-24 17:22:33 -0400

    Removed boilerplate from index.html and made the Q logo a configurable component

--- src/components/side-menu/SideMenu.js
+++ src/components/side-menu/SideMenu.js
@@ -1,7 +1,10 @@
 import "./SideMenu.css";
+import Logo from "../Logo";
 
 function SideMenu() {
-  return <div>Side Menu</div>;
+  return (
+    <Logo height="100" width="100"color="rgb(221,34,155)"/>
+  );
 }
 
 export default SideMenu;
