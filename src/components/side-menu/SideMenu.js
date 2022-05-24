a89268e7c409d62e5bf9c2b03126a532a053d87a
Corbin McGuire <mcguire.corbin@gmail.com>
2022-05-24 17:27:51 -0400

    formatting

--- src/components/side-menu/SideMenu.js
+++ src/components/side-menu/SideMenu.js
@@ -2,9 +2,7 @@ import "./SideMenu.css";
 import Logo from "../Logo";
 
 function SideMenu() {
-  return (
-    <Logo height="100" width="100"color="rgb(221,34,155)"/>
-  );
+  return <Logo height="100" width="100" color="rgb(221,34,155)" />;
 }
 
 export default SideMenu;
