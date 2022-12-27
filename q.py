0c50712f069068f3f0590cbdc5650fd979eba9be
Cogan Shimizu <cog-academics@coganshimizu.com>
2022-12-27 10:18:13 -0500

    switched to bot from client

--- q.py
+++ q.py
@@ -2,21 +2,16 @@
 import os
 
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client(intents=discord.Intents.default())
-tree = discord.app_commands.CommandTree(client)
+bot = commands.Bot(command_prefix="/", intents=discord.Intents.default())
 
-@client.event
+@bot.event
 async def on_ready():
-    await tree.sync()
-    print(f'{client.user} has connected to Discord!')
+    print(f'{bot.user} has connected to Discord!')
 
-@tree.command(name = "commandname", description = "My first application Command") 
-async def first_command(interaction):
-    await interaction.response.send_message("Hello!")
-
-client.run(TOKEN)
+bot.run(TOKEN)
