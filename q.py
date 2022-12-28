80adbe33e529f55e2fd00f2c473fabd2855852b4
Cogan Shimizu <cog-academics@coganshimizu.com>
2022-12-27 21:24:28 -0500

    added imports for each command

--- q.py
+++ q.py
@@ -1,17 +1,43 @@
-# bot.py
-import os
+#############
+#############
+##### Q #####
+#############
+#############
+        ####
+          ###
 
+# For loading the secrets
+import os, sys
+from dotenv import load_dotenv
+import inspect
+# Discord libraries
 import discord
 from discord.ext import commands
-from dotenv import load_dotenv
 
+# Import commands for q
+from qcommands import help
+from qcommands import myq
+from qcommands import enq
+from qcommands import deq
+from qcommands import getq
+from qcommands import register
+
+
+# Load the secrets
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
+# Start the bot
 bot = commands.Bot(command_prefix="/", intents=discord.Intents.default())
 
+# Register cogs for commands
+# await bot.add_cog(Help(bot))
+
+# Signal that the bot has connected
 @bot.event
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
+    # await bot.add_cog(help.HelpCog(bot))
 
+# Start the bot!
 bot.run(TOKEN)
