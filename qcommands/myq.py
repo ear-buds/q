3799ea93ffc032257925a228d99bccb9bae70fee
Cogan Shimizu <cog-academics@coganshimizu.com>
2022-12-27 21:25:47 -0500

    fixed error

--- qcommands/myq.py
+++ qcommands/myq.py
@@ -5,12 +5,12 @@ async def first_command(interaction):
 import discord
 from discord.ext import commands
 
-class EnqCog(commands.Cog):
+class MyqCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
     @commands.command()
-    async def enq(self, ctx):
+    async def myq(self, ctx):
         embed = discord.Embed(title="Help commands", description="Shows various help commands")
         embed.add_field(name="Show this help", value = "`/help`", inline = False)
         await ctx.send(embed=embed)
