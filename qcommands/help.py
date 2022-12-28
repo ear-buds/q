701ff6e84e9c638a608c0e43b78f75b1801495f0
Cogan Shimizu <cog-academics@coganshimizu.com>
2022-12-27 21:24:43 -0500

    switched to cog style, boilerplate, stubs

--- qcommands/help.py
+++ qcommands/help.py
@@ -9,7 +9,4 @@ class HelpCog(commands.Cog):
     async def help(self, ctx):
         embed = discord.Embed(title="Help commands", description="Shows various help commands")
         embed.add_field(name="Show this help", value = "`/help`", inline = False)
-        embed.add_field(name="Help random stuff", value="`/helprand`", inline=False)
-        embed.add_field(name="Help economy", value="`/helpeco`", inline=False)
-        embed.add_field(name="Help music", value = "`/helpmusic`", inline = False)
         await ctx.send(embed=embed)
--- qcommands/myq.py
+++ qcommands/myq.py
@@ -1,3 +1,16 @@
 @tree.command(name = "myq", description = "My first application Command") 
 async def first_command(interaction):
     await interaction.response.send_message("`myq` called!")
+
+import discord
+from discord.ext import commands
+
+class EnqCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.command()
+    async def enq(self, ctx):
+        embed = discord.Embed(title="Help commands", description="Shows various help commands")
+        embed.add_field(name="Show this help", value = "`/help`", inline = False)
+        await ctx.send(embed=embed)
